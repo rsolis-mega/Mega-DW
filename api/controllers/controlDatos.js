e9a8e84cedf63e83aaec294b0b90e27420a61240
rsolis-mega <87153152+rsolis-mega@users.noreply.github.com>
2021-11-16 10:02:28 -0600

    Petición de Ticket por API

--- api/controllers/controlDatos.js
+++ api/controllers/controlDatos.js
@@ -1,9 +1,31 @@
-const fetch = require('node-fetch')
+'use strict'
 
-const peticion = async (req, res, next) => {
-    console.log('Hola')
+const axios = require('axios')
+// const { default: axios } = require('axios')
+const { json } = require('body-parser')
+const { response } = require('express')
+
+const getTicket1 = async(req, res, next) => {
+    try {
+        let respuesta = await getTicket()
+        res.send(respuesta)
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+}
+
+async function getTicket() {
+    try {
+        const username = 'AdministradorIT'
+        let res = await axios.post(`http://192.168.25.17/trusted?username=${username}`)
+        let datos = res.data
+        console.log('Token: ', datos)
+        return datos
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 module.exports = {
-    peticion
+    getTicket1
 }
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,7 +4,7 @@ const express = require('express')
 const controlDatos = require('../controllers/controlDatos')
 const route = express.Router()
 
-route.get('/get-ticket', controlDatos.peticion)
+route.get('/get-ticket', controlDatos.getTicket1)
 
 module.exports = {
     route: route
