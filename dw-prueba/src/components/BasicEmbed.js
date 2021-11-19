119c1569f6af917b33d13f26c2088106348d03a5
rsolis-mega <87153152+rsolis-mega@users.noreply.github.com>
2021-11-19 15:54:19 -0600

    Update BasicEmbed.js

--- dw-prueba/src/components/BasicEmbed.js
+++ dw-prueba/src/components/BasicEmbed.js
@@ -1,39 +1,4 @@
-/* import React, { Component } from 'react'
-import Iframe from 'react-iframe'
-import tableauSoftware from 'tableau-api'
-
-const url = 'http://192.168.25.17/views/Datosgenerales/Dashboard2?:origin=card_share_link&:embed=n'
-const frame = <Iframe url="http://192.168.25.17/trusted//views/workbookQ4/SalesQ4?:embed=yes"></Iframe>
-
-
-class BasicEmbed extends Component {
-
-    componentDidMount() {
-        this.initViz()
-    }
-    
-
-    initViz = () => {
-        const options = {
-            hideTabs: false,
-            width: '1280px',
-            height: '720px',
-        }
-        const vizContainer = this.vizContainer
-        this.div = new window.tableauSoftware.Viz(vizContainer, url, options)
-    }
-
-    render() {
-        return (
-            <div ref = {div => this.vizContainer = div}>
-                
-            </div>
-        )
-    }
-} */
-
 import React, { Component } from 'react'
-import { ReactDOM } from 'react-dom'
 
 export default class BasicEmbed extends Component {
     render() {
@@ -44,5 +9,3 @@ export default class BasicEmbed extends Component {
         )
     }
 }
-
-// export default BasicEmbed
