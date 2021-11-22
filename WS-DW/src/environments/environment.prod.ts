7fa6b7e63d8b68ee0e049dd186717250e9a687f9
rsolis-mega <87153152+rsolis-mega@users.noreply.github.com>
2021-11-22 15:26:33 -0600

    Corrección llamada api

--- WS-DW/src/environments/environment.prod.ts
+++ WS-DW/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //apiProductiva: 'http://192.168.25.34',
-  SERVICIOS_DW: 'http://localhost:8080/api'
+  SERVICIOS_DW: 'http://192.168.25.34:8080/api',
+  //SERVICIOS_DW: 'http://localhost:8080/api'
 };
